679ae26fb235eb4a15bcebeb2171b7f5b490fc9e
anasfathysalama <salamaanas681@gmail.com>
2022-12-23 06:40:24 +0200

    modify picked up parcel by biker api

--- app/Http/Controllers/api/BikerController.php
+++ app/Http/Controllers/api/BikerController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Actions\StoreOrderAction;
-use App\Actions\UpdateParcelAction;
+
 use App\Http\Controllers\Controller;
-use App\Http\Requests\PickedParcelRequest;
 use App\Models\Parcel;
 use App\Traits\ApiResponseTrait;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class BikerController extends Controller
 {
