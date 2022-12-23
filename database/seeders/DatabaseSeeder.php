53c779d10ec4b4e695e35f049198617ebfd6442f
anasfathysalama <salamaanas681@gmail.com>
2022-12-23 06:17:01 +0200

    create seeder to seed users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                'name' => "biker_{$i}",
+                'username' => "biker_{$i}",
+                'password' => Hash::make('123456'),
+                'type' => 'biker'
+            ]);
+        }
+
+        for ($i = 1; $i <= 5; $i++) {
+            User::create([
+                'name' => "sender_{$i}",
+                'username' => "sender_{$i}",
+                'password' => Hash::make('123456'),
+                'type' => 'sender'
+            ]);
+        }
     }
 }
